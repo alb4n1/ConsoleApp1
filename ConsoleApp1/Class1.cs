4b6b6dad4f6110858aec8c66eb8d34d28230ce87
student <student@LAB05-30>
2022-11-03 18:54:03 +0100

    Add project files.

---
+++ ConsoleApp1/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Class1
+    {
+        public string Emri { get; set; }
+
+        public string Mbiemri { get; set; }
+
+        public int Mosha { get; set; }
+
+        public string Adresa { get; set; }
+
+        void Pershendete()
+        {
+            Console.WriteLine($"tungjatjeta {Emri} {Mbiemri}");
+        }
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,9 @@
+﻿
+//Console.WriteLine("Kolegji AAB");
+
+//var puntoret = new List<string
+//var numri = 10;
+//foreach (var puntori :string  in puntoret)
+//    {
+//    Console.WriteLine(puntoret);
+//
